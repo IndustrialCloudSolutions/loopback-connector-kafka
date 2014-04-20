cafb96c9b1f0a4e890953f4701b4a47d7e74fc58
haio <mockingror@gmail.com>
2014-04-20 12:09:17 +0800

    configure remote method Message.send

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-module.exports = require('./lib/kafka');
+var loopback = require('loopback');
+var KafkaConnector = require('./lib/kafka');
+
+function createMessageModel (options) { 
+    options = options || {};
+    var dataSource = loopback.createDataSource({
+        connector: KafkaConnector,
+        connectionString: options.connectionString,
+        clientId: options.clientId,
+        zkOptions: options.zkOptions
+    });
+    var model = dataSource.createModel(options.name || 'Message', {}, {plural: options.plural || 'messages'});
+    return model;
+}
+exports = module.exports = KafkaConnector;
+exports.Message = createMessageModel();
